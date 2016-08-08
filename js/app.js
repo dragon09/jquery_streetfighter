d58c87e3560aa67a1303278617379f5f32c36faf
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-08-08 12:33:26 -0500

    action changes

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     /* Act on the event */
     // alert('mouse entered .ryu div');
-    $('.ryu-still').hide();
+    // $('.ryu-still').hide();
+    $('.action').hide();
     $('.ryu-ready').show();
     // chain menthod no semicolon
   })
   .mouseleave(function() {
     /* Act on the event */
     // alert('mouse left');
-    $('.ryu-ready').hide();
+    $('.ryu-action').hide();
+    // $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
   .mousedown(function() {
@@ -27,18 +29,39 @@ $(document).ready(function() {
     $('.ryu-throwing').show();
     // $('.hadouken').show();
     $('.hadouken').finish().show().animate(
-      {'left': '1020px'},
+      // {'left': '1020px'},
+      {'left': '300px'},
       500,
       function() {
-        $(this).hide();
-        $(this).css('left', '520px');
+        $('.hadouken').stop();
+        $('.hadouken').hide();
+        $('.hadouken').css('left', '-212px');
+        // $(this).hide();
+        // $(this).css('left', '520px');
       }
     );
   })
   .mouseup(function() {
     /* Act on the event */
     // console.log('mouse up');
-    $('.ryu-throwing').hide();
+    // $('.ryu-throwing').hide();
+    $('.ryu-action').hide();
     $('.ryu-ready').show();
   });
-});
+
+  $(document).keydown(function(e) {
+    // alert( "Handler for .keydown() called." );
+    if (e.keyCode == 88) {
+      playCool();
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }
+  }).keyup(function(e) {
+    if(e.keyCode == 88) {
+      $('#cool')[0].pause();
+      $('#cool')[0].load();
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+}

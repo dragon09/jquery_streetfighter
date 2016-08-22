4fa4e2695b8198472e6fb2655a4fd296fd895819
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-08-21 22:42:40 -0500

    sound intro css

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
+  playStreetfighter();
+  musicIntro();
+});
 
+var hadoukenSound = false;
   function playHadouken() {
+    hadoukenSound = !hadoukenSound;
+    if (hadoukenSound) {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
   }
+}
 
+  var coolSound = false;
+function playCool () {
+  coolSound = !coolSound;
+  if (coolSound) {
+    $('#street-fighter-theme')[0].pause();
+    // $('#cool')[0].load()
+    $('#cool')[0].play();
+  }
+}
+
+function playStreetfighter() {
   $('.ryu').mouseenter(function() {
     /* Act on the event */
     // alert('mouse entered .ryu div');
@@ -54,16 +72,54 @@ $(document).ready(function() {
   $(document).keydown(function(e) {
     // alert( "Handler for .keydown() called." );
     if (e.keyCode == 88) {
-      // playCool();
+      playCool();
       $('.ryu-action').hide();
       $('.ryu-cool').show();
     }
   }).keyup(function(e) {
     if(e.keyCode == 88) {
-      // $('#cool')[0].pause();
-      // $('#cool')[0].load();
+      $('#cool')[0].pause();
+      $('#cool')[0].load();
       $('.ryu-cool').hide();
       $('.ryu-still').show();
     }
   });
-});
+}
+
+function musicIntro() {
+  $('#street-fighter-theme')[0].volume = 0.5;
+  // $('#street-fighter-theme').load();
+  $('#street-fighter-theme')[0].play();
+  $('.sf-logo').toggle(5000, function() {
+  $('.sf-logo').fadeOut(3500, function() {
+    $('.brought-by').fadeIn(1500, function() {
+      $('.brought-by').fadeOut(1500, function() {
+        $('.jquery-logo').fadeIn(1500, function() {
+          $('.jquery-logo').fadeOut(1000, function() {
+            $('.instructions').fadeIn(1000);
+
+            });
+
+          })
+
+        })
+
+      })
+
+    })
+
+  })
+
+}
+// }).keyup(function(e) {
+    // if(e.keyCode == 13) {
+    //   $('.ryu-jump-kick').hide();
+    //   // $('.ryu-still').show();
+    // }
+  // });
+
+  // $( "instructions" ).click(function() {
+  //   $( "p" ).slideToggle( "slow" );
+  // });
+
+// });
